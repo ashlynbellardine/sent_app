efe951db260f84c7061cc211d897c852dfb5d06d
Ashlyn Bellardine <amb655@georgetown.edu>
2023-12-12 22:39:10 -0500

    new virt

--- app.py
+++ app.py
@@ -1,55 +1,5 @@
-# import streamlit as st
-# from textblob import TextBlob  # sentiment analysis package
-# import plotly.graph_objects as go  # data visualization package
-
-# st.markdown("# Welcome to my app!")
-
-# st.markdown("### Please enter some text below and press enter!")
-
-# st.markdown("### Pretty please!")
-
-
-# def sent_app(text):
-#     # Extract sentiment
-#     sentiment_score = TextBlob(f"{text}").sentiment.polarity
-
-#     # Use score to assign label
-#     if sentiment_score > 0.15:
-#         label = "Positive"
-#     elif sentiment_score < -0.15:
-#         label = "Negative"
-#     else:
-#         label = "Neutral"
-
-#     # Plot score on gauge plot
-#     fig = go.Figure(
-#         go.Indicator(
-#             mode="gauge+number",
-#             value=sentiment_score,
-#             title={"text": f"Sentiment: {label}"},
-#             gauge={
-#                 "axis": {"range": [-1, 1]},
-#                 "steps": [
-#                     {"range": [-1, -0.15], "color": "red"},
-#                     {"range": [-0.15, 0.15], "color": "gray"},
-#                     {"range": [0.15, 1], "color": "lightgreen"},
-#                 ],
-#                 "bar": {"color": "yellow"},
-#             },
-#         )
-#     )
-
-#     return st.plotly_chart(fig)
-
-
-# text = st.text_input("Enter text:", value="Enter text here")
-
-# sent_app(text)
-
-
 import numpy as np
 import pandas as pd
-import altair as alt
 import streamlit as st
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
